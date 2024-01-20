e69858fe4b8bf2c66f59bb544f53d68b90fa6d7c
Alvaro Armijos <alvaro.armijoss@gmail.com>
2024-01-20 17:20:11 -0500

    Create push notifications method.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin")
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+
+exports.pushNotification = functions.database.ref('/chats/{chatId}/messages/{id}').onCreate((snap, context) => {
+    console.log('Push notification event triggered');
+
+    //  Get the current value of what was written to the Realtime Database.
+    const valueObject = snap.val();
+
+    const sentTo = valueObject.sentTo;
+    const sentBy = valueObject.sentBy;
+
+    const name = admin.database().ref(`/status/${sentBy}/name`).once('value');
+    const token = admin.database().ref(`/status/${sentTo}/token`).once('value');
+
+    Promise.all([name, token]).then(async (values) => {
+
+        const token = values[1].val();
+
+        if (token == null) return { error: "No token" };
+        // Create a notification
+        const payload = {
+            token: token,
+            notification: {
+                title: values[0].val(),
+                body: valueObject.message,
+            }
+        };
+
+        try {
+            const response = await admin.messaging().send(payload);
+            // Response is a message ID string.
+            console.log('Successfully sent message:', response);
+            return { success: true };
+        } catch (error) {
+            console.log('error', error);
+            return { error: error.code };
+        }
+    });
+});
